48246254a77384b2da40e1c81ae5e0a4192510a1
jasiafaruksaba <sabajasiafaruk@gmail.com>
2026-08-02 21:46:37 +0600

    evenodd files

--- problem-solving.ts
+++ problem-solving.ts
@@ -6,3 +6,18 @@ const reverseString = (str: string): string =>{
     return str.split("").reverse().join("");
 };
 console.log(reverseString("hello"))
+
+
+// ================================
+// Problem 02: Even or Odd
+// ================================
+
+const checkEvenOdd = (num: number): string =>{
+    if (num % 2 === 0) {
+       return "Even" ;
+    }
+
+    return "Odd";
+};
+
+console.log(checkEvenOdd(7));
