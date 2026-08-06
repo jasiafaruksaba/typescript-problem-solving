31d4c3221b55cbb86d8c90f466f3ba08732955ef
jasiafaruksaba <sabajasiafaruk@gmail.com>
2026-08-06 22:11:27 +0600

    Checking Prime Number file

--- problem-solving.ts
+++ problem-solving.ts
@@ -114,3 +114,23 @@ const factorial = (n: number): number => {
 
 console.log(factorial(5));
 
+// ==========================================
+// Problem 08: Checking Prime Number
+// ===========================================
+
+const isPrime = (n: number): boolean => {
+  if (n < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+console.log(isPrime(7));
+console.log(isPrime(10));
